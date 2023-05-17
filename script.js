19fc3ca3c1382fd81881429c11b2482f138e1a33
anuragbhardwaj21 <anurag.bhardwaj_cs18@gla.ac.in>
2023-05-17 17:06:15 +0530

    Homepage

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-import { navbar, footer, commentData, data} from "./components/components.js";
+import {
+  navbar,
+  footer,
+  commentData,
+  data,
+  reviewCard,
+  greenCard,
+} from "./components/components.js";
+
+// -----------------------------------------------------------------------------navbar,footer ↓
 
 var navbarArea = document.getElementById("navbar");
 navbarArea.innerHTML = navbar();
@@ -6,20 +15,81 @@ navbarArea.innerHTML = navbar();
 var footerArea = document.getElementById("footer");
 footerArea.innerHTML = footer();
 
+// -----------------------------------------------------------------------------cards section ↓
 
+var cardsection = document.getElementById("cardsection");
 
-var cardsection=document.getElementById("cardsection")
-console.log(commentData)
+for (let i = 0; i < data.length; i++) {
+  var card = document.createElement("div");
+  card.setAttribute("id", "m-b-card");
 
-for(let i=0;i<data.length;i++){
-    var card=document.createElement("div")
-    card.setAttribute("id","m-b-card")
-
-    card.innerHTML=`
+  card.innerHTML = `
     <img src="${data[i].link}" alt="">
     <p>${data[i].name}</p>
-    `
-    cardsection.append(card)
+    `;
+  cardsection.append(card);
 }
 
-var storiesCardSection=document.getElementById("stories-card-section")
+// -----------------------------------------------------------------------------green banner section ↓
+
+var greenbanner1 = document.getElementById("greenbanner1");
+for (let i = 0; i < 4; i++) {
+  var greenBannerCard = document.createElement("div");
+  greenBannerCard.setAttribute("id", "greenbanner1-card");
+  greenBannerCard.innerHTML = greenCard(i);
+  greenbanner1.append(greenBannerCard);
+}
+
+// -----------------------------------------------------------------------------story section ↓
+
+var storiesCardSection = document.getElementById("stories-card-section");
+
+let currentCard = null;
+
+function renderCard(card, cardContent) {
+  if (card === currentCard) {
+    cardContent.style.display = "none";
+    card.style.width = "2%";
+
+    setTimeout(() => {
+      cardContent.style.display = "none";
+    }, 300);
+
+    currentCard = null;
+  } else {
+    if (currentCard) {
+      currentCard.querySelector(".card-content").style.display = "none";
+      currentCard.style.width = "2%";
+      currentCard.querySelector(".card-content").style.opacity = "0";
+    }
+
+    card.style.width = "18%";
+
+    setTimeout(() => {
+      cardContent.style.display = "block";
+      cardContent.style.opacity = "1";
+    }, 700);
+
+    currentCard = card;
+  }
+}
+
+for (let i = 0; i < 12; i++) {
+  (function (index) {
+    var card = document.createElement("div");
+    card.setAttribute("id", `stories-card-${index}`);
+    var cardContent = document.createElement("div");
+    cardContent.setAttribute("id", `card-content-${index}`);
+    card.append(cardContent);
+
+    card.addEventListener("click", function () {
+      renderCard(card, cardContent);
+    });
+
+    card.classList.add(`stories-card`);
+    cardContent.classList.add(`card-content`);
+
+    cardContent.innerHTML = reviewCard(i);
+    storiesCardSection.append(card);
+  })(i);
+}
